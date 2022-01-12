e0e150fe9c64f51524000ecf679b6c6349a2c4b7
mdbrown543 <mdbrown543@gmail.com>
2022-01-12 15:18:11 -0600

    Added new files

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // takes an array as an argument so need a parameter and
 // returns 10 times the array length for 10 minutes per exercise
-function calculateWorkoutLength() {}
+/*function calculateWorkoutLength(arr) {
+    return arr.length * 10;
+}*/
 
+calculateWorkoutLength = arr => arr.length * 10;
 // takes two arguments an array and a number of exercises and
 // returns a new array generated with random exercises based on
 // the number passed into the parameter
-function generateRandomExercises() {}
+generateRandomExercises = (arr, num) => {
+    let workout = [];
+    
+    for(let i = 0;i < num;i++){
+        let random = Math.floor(Math.random() * arr.length);
+
+        workout.push(arr[random]);
+        
+    }
+    return workout;
+}
+
+console.log(generateRandomExercises(exercises,10));
